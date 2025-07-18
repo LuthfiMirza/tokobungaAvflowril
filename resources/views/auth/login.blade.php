0727beca8e718c58a3d60475b7c2b44db8230509
LuthfiMirza <luthfimirza@example.com>
2025-07-18 10:17:40 +0700

    fix bug on login and shop view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,12 +10,6 @@
             <div class="col-12">
                 <div class="breadcrumb-content text-center">
                     <h1 class="breadcrumb-title">Login</h1>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb justify-content-center">
-                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Login</li>
-                        </ol>
-                    </nav>
                 </div>
             </div>
         </div>
